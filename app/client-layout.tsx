f604a9d9865b2cda39057baf9bba494fc2dd2e0e
Andrej Koller <andrejkoller@outlook.com>
2026-07-18 17:49:55 +0200

    refactor: simplify ClientLayout and remove unused components

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -1,21 +1,9 @@
 "use client";
 
-import { Header } from "@/components/header";
-import { ThemeProvider } from "@/components/theme/theme-provider";
-import { ThemeSwitcher } from "@/components/theme/theme-switcher";
-
 export default function ClientLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <ThemeProvider>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main>{children}</main>
-        <ThemeSwitcher />
-      </div>
-    </ThemeProvider>
-  );
+  return <main className="flex flex-col min-h-screen">{children}</main>;
 }
